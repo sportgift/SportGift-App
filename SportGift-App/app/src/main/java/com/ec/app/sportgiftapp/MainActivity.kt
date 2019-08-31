8d4f85a46d0fc4513ebac2f18ae219eebf2a9166
sportgift developer <sport.gift.io@gmail.com>
2019-08-31 07:59:29 -0500

    Initial Commit

---
+++ SportGift-App/app/src/main/java/com/ec/app/sportgiftapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ec.app.sportgiftapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
