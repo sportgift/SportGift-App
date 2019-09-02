d60255a863755d27317bdd5cf30d76b82b7d04d6
sportgift developer <sport.gift.io@gmail.com>
2019-09-01 23:04:12 -0500

    Update Activity

--- SportGift-App/app/src/main/java/com/ec/app/sportgiftapp/MainActivity.kt
+++ SportGift-App/app/src/main/java/com/ec/app/sportgiftapp/MainActivity.kt
@@ -14,7 +14,6 @@ import android.view.animation.AccelerateInterpolator
 import android.view.animation.BounceInterpolator
 import android.webkit.*
 import android.widget.ImageView
-import com.ec.app.sportgiftapp.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     private var ball: ImageView? = null
     private var image: ImageView? = null
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar!!.hide()
         setContentView(R.layout.activity_main)
@@ -36,7 +35,53 @@ class MainActivity : AppCompatActivity() {
         animate1()
         animate2()
 
-            
+        // Get the web view settings instance
+        val settings = web_view.settings
+
+        // Enable java script in web view
+        settings.javaScriptEnabled = true
+
+        // More web view settings
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            settings.safeBrowsingEnabled = true  // api 26
+        }
+        //settings.pluginState = WebSettings.PluginState.ON
+        settings.useWideViewPort = true
+        settings.loadWithOverviewMode = true
+        settings.javaScriptCanOpenWindowsAutomatically = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            settings.mediaPlaybackRequiresUserGesture = false
+        }
+
+        web_view.loadUrl(url);
+
+        // Set web view client
+        web_view.webViewClient = object: WebViewClient(){
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                // Page loading started
+                // Do something
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                // Page loading finished
+            }
+        }
+
+        // Set web view chrome client
+        web_view.webChromeClient = object: WebChromeClient(){
+            override fun onProgressChanged(view: WebView, newProgress: Int) {
+                progress_bar.progress = newProgress
+
+                //display web page
+                if (newProgress > 99){
+                    hlnt_iv_load.visibility = View.GONE
+                    logo.visibility = View.GONE
+                    web_view.visibility = View.VISIBLE
+                    supportActionBar!!.show()
+                }
+                Log.d("Progress",newProgress.toString())
+            }
+        }
     }
 
     private fun animate1(){
