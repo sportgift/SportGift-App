a908c0abb6b8ee43701e6fa47647839c1c323f4b
jordid <jordiduranpesantez@gmail.com>
2019-10-29 22:48:18 -0500

    trabajando en el logn

--- sportGift/src/app/app.module.ts
+++ sportGift/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './public/login/login.component';
+import { BodyComponent } from './public/body/body.component';
+import { FooterComponent } from './public/footer/footer.component';
+import { HeaderComponent } from './public/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BodyComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
